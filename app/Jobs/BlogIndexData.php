f64121fdbd6bfbe152ad14b1ca69ee072858550b
Lee Ibrahim <leeibrah@gmail.com>
2020-05-23 17:56:27 +0300

    remove published at

--- app/Jobs/BlogIndexData.php
+++ app/Jobs/BlogIndexData.php
@@ -43,7 +43,6 @@ class BlogIndexData extends Job implements SelfHandling
     protected function normalIndexData()
     {
         $posts = Post::with('tags')
-            ->where('published_at', '<=', Carbon::now())
             ->where('is_draft', 0)
             ->orderBy('published_at', 'desc')
             ->simplePaginate(config('blog.posts_per_page'));
