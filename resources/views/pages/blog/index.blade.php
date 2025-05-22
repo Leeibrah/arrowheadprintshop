465f0b14b3256a1cdd41dae1901bddd70e636e1d
Lee Ibrahim <leeibrah@gmail.com>
2025-05-22 19:35:47 +0300

    sort blog section - 01

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -23,63 +23,46 @@
         </div>
     </section>
     <!-- Breadcrumbs Section -->
-   
-   <!-- End medical blog list area -->
-    <section class="blog_list_area sec_pad">
-        <div class="container custom_container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="blog_main">
 
-                        @foreach ($posts as $post)
-                            <div class="team_members text-center wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                                <div class="row">
-                                    <div class="blog_post_item  col-lg-4 col-md-6">
-                                        <div class="post_media">
-                                    
-                                            
-                                                <a href="/blog/{!! $post->slug !!}">
-                                                    <img src="/uploads/{!! $post->page_image !!}" alt="Blog">
-                                                </a>
-                                            
-                                            <div class="post_date">
-                                                <span>
-                                                    {!! $post->published_at->format('F j, Y') !!}
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div class="media_blog_content">
-                                            <a href="/blog/{!! $post->slug !!}">
-                                                <b class="blog_title">{!! $post->title !!}</b>
-                                            </a>
-                                            <p>
-                                                {!! substr($post->content_raw, 0, 160) !!} ... 
-                                            </p>
-                                            <div class="post_bottom">
-                                                <a href="/blog/{!! $post->slug !!}" class="learn_btn_two">Read More</a>
-                                            </div>
-                                        </div>
-                                    </div>
+
+    <!-- Info Section -->
+    <section class="info_box_aria about">
+        <div class="container custom_container">
+
+            <div class="info_box_items text-center">
+                <div class="row">
+
+                    @foreach ($posts as $post)
+                        <div class="col-lg-4 col-md-6" style="margin-top: 3%">
+                            <div class="single_item">
+                                
+                                <a href="/blog/{!! $post->slug !!}">
+                                    <img src="/uploads/{!! $post->page_image !!}" alt="{!! $post->title !!}">
+                                </a>
+
+                                <div class="content">
+                                    <h4>{!! $post->title !!}</h4>
+                                    <p>
+                                        {!! $post->published_at->format('F j, Y') !!}
+                                    </p>
+                                    <!-- <a href="service.html" class="btn_2">Learn more</a> -->
+                                </div>
+
+                                <div class="post_bottom">
+                                    <a href="/blog/{!! $post->slug !!}" class="learn_btn_two">Read More</a>
                                 </div>
                             </div>
-                        @endforeach
+                        </div>
+                    @endforeach
 
-                        <!-- <nav class="navigation pagination" role="navigation">
-                            <div class="nav-links">
-                                <span aria-current="page" class="page-numbers current">1</span>
-                                <a class="page-numbers" href="#">2</a>
-                                <a class="next page-numbers" href="#"><i class="fa fa-arrow-right"></i></a>
-                            </div>
-                        </nav> -->
-                    </div>
                 </div>
-                
-                <!-- SIDEBAR -->
-                    {{-- @include('partials._blog_sidebar') --}}
-                <!-- /. SIDEBAR -->
             </div>
         </div>
+        <div class="shape">
+            <img src="/assets/img/shape/info_box_3.png" alt="" class="one_op2">
+        </div>
     </section>
-    <!-- End medical blog list area -->
+    <!-- Info Section -->
+   
 
 @endsection
